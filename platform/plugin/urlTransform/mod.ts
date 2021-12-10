b892c836aa29534f6e8fa1ede2f27649b90b831e
Heesun Jun <hjun@yext.com>
2021-12-10 16:59:03 -0500

    update var name to domain

--- platform/plugin/urlTransform/mod.ts
+++ platform/plugin/urlTransform/mod.ts
@@ -1,9 +1,9 @@
-declare var domainHost: string;
+declare var domain: string;
 
 export function fix_avatar_template(avatar_url: string) {
     let re = /,\//gi
     let re2 = /^\//gi
     let httpRegex = /https?:\/\/|\/.*/gi
-    var sanitizedDomainHost = "https://" + domainHost.replace(httpRegex, '')
+    var sanitizedDomainHost = "https://" + domain.replace(httpRegex, '')
     return avatar_url.replace(re, "," + sanitizedDomainHost + "/").replace(re2, sanitizedDomainHost + "/")
 }
